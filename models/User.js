d76906712f9c55116617c790251c3844fa05e37d
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-05-23 19:24:14 -0600

    add user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,56 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const UserSchema = new Schema
+const UserSchema = new Schema(
+  {
+    //username: string unique required trimmed
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    //email: string required unique must match valid email
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "Email validation failed",
+      },
+    },
+    //thoughts: id values matching thoughts
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    // friends
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+const User = model('User', UserSchema);
+
+module.exports = User;
